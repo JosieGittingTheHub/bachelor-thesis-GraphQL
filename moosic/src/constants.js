010cdaaa0fe04b709070db11c99c81546da8c42a
JosieGittingTheHub <josefine.hartung@gmail.com>
2018-03-27 10:13:11 +0200

    Add moosic react app

--- moosic/src/constants.js
+++ moosic/src/constants.js
@@ -1,1 +1,3 @@
-export const AUTH_TOKEN = 'auth-token'
+var client_id = '420099d36fef478e9dfff118a4802694'; // Your client id
+var client_secret = '9220564ffd8849d79a1d232784ca3990'; // Your secret
+var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
--- moosic/src/index.js
+++ moosic/src/index.js
@@ -1,47 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/index.css'
-import App from './components/App'
-import registerServiceWorker from './registerServiceWorker'
-// 1
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { BrowserRouter } from 'react-router-dom'
-import { AUTH_TOKEN } from './constants'
-import { ApolloLink } from 'apollo-client-preset'
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App';
+import { Router, IndexRoute, Route, hashHistory } from 'react-router';
+import Login from './scenes/LoginScene';
+import TagScene from './scenes/TagScene';
+import ResultScene from './scenes/ResultScene';
+import Error from './scenes/Error';
 
-// 2
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
-
-const middlewareAuthLink = new ApolloLink((operation, forward) => {
-  const token = localStorage.getItem(AUTH_TOKEN)
-  const authorizationHeader = token ? `Bearer ${token}` : null
-  operation.setContext({
-    headers: {
-      authorization: authorizationHeader
-    }
-  })
-  return forward(operation)
-})
-
-const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
-
-// 3
-const client = new ApolloClient({
-  link: httpLinkWithAuthToken,
-  cache: new InMemoryCache()
-})
-
-// 4
 ReactDOM.render(
-  <BrowserRouter>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </BrowserRouter>,
-  document.getElementById('root'),
+        <Router history={hashHistory}>
+          <Route path="/" component={App}>
+            <IndexRoute component={Login} />
+            <Route path="/tags" component={TagScene} />
+            <Route path="/results" component={ResultScene} />
+            <Route path="/error/:errorMsg" component={Error} />
+          </Route>
+        </Router>
+    , document.getElementById('root')
 )
-registerServiceWorker()
-
